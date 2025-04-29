69685c4fcc05c94456a3e238c797646b52a2a71c
JosueLop1221 <josuelopezz1221@gmail.com>
2025-04-29 12:40:54 -0600

    Mi Nuevo Proyecto Desde Cero

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5vuvTfN8Ql_Js19rgnDjazA3177KvcDA",
-  authDomain: "castillo-it-da835.firebaseapp.com",
-  projectId: "castillo-it-da835",
-  storageBucket: "castillo-it-da835.firebasestorage.app",
-  messagingSenderId: "11441382403",
-  appId: "1:11441382403:web:45827822c82976fc1d9a36",
-  measurementId: "G-MCBB0LMR8R"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
